32dd2d75094e79b1a724de7b17cfe3c61e7b6ef1
KotteJR <aleksandar.kotevski@adamass.se>
2025-04-15 16:06:15 +0200

    alskjd

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const nextConfig = {
   swcMinify: true,
   // Configure for local file storage
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'voxstudios.blob.core.windows.net',
+        port: '',
+        pathname: '/videos/**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '50mb',
+    },
+    responseLimit: '50mb',
+  },
+  serverless: {
+    maxDuration: 60, // Set to maximum allowed value for Hobby plan
+  },
 };
 
 module.exports = nextConfig; 
